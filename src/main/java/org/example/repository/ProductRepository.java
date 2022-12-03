86f092a0938761333f092a917cc157b31ccde216
Ivan Zuev <ivan.zuev96@yandex.ru>
2022-12-03 14:00:40 +0300

    first commit

---
+++ src/main/java/org/example/repository/ProductRepository.java
@@ -0,0 +1,53 @@
+package org.example.repository;
+
+import org.example.domain.Product;
+import org.example.exception.AlreadyExistsException;
+import org.example.exception.NotFoundException;
+
+public class ProductRepository {
+    private Product[] products = new Product[0];
+
+    public void save(Product product) {
+        if (findById(product.getId()) != null) {
+            throw new AlreadyExistsException(
+                    "Element with id: " + product.getId() + " already exist"
+            );
+        }
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+    }
+
+    public Product[] findAll() {
+        return products;
+    }
+
+    public void removeById(int id) {
+        if (findById(id) == null) {
+            throw new NotFoundException(
+                    "Element with id: " + id + " not found"
+            );
+        }
+        Product[] tmp = new Product[products.length - 1];
+        int index = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[index] = product;
+                index++;
+            }
+        }
+        products = tmp;
+    }
+
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/org/example/ProductRepositoryTest.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.example.domain.Book;
+import org.example.domain.Product;
+import org.example.domain.Smartphone;
+import org.example.exception.AlreadyExistsException;
+import org.example.exception.NotFoundException;
+import org.example.repository.ProductRepository;
+
+public class ProductRepositoryTest {
+    ProductRepository repo = new ProductRepository();
+
+    Product product1 = new Book(3, "Book 1", 120, "Author 1");
+    Product product2 = new Book(8, "Book 2", 110, "Author 2");
+    Product product3 = new Book(11, "Book 3", 115, "Author 3");
+    Product product4 = new Smartphone(12, "Smartphone 1", 120, "Producer 1");
+    Product product5 = new Smartphone(16, "Smartphone 2", 95, "Producer 2");
+    Product product6 = new Smartphone(19, "Smartphone 3", 9500, "Producer 3");
+
+    @BeforeEach
+    public void setup() {
+        repo.save(product1);
+        repo.save(product2);
+        repo.save(product3);
+        repo.save(product4);
+        repo.save(product5);
+        repo.save(product6);
+    }
+
+    @Test
+    public void shouldRemoveById() {
+        repo.removeById(12);
+
+        Product[] expected = {product1, product2, product3, product5, product6};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldExceptionRemoveById() {
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.removeById(15);
+        });
+    }
+
+    @Test
+    public void shouldSaveProduct() {
+
+        Product[] expected = {product1, product2, product3, product4, product5, product6};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldExceptionSaveProduct() {
+
+        Assertions.assertThrows(AlreadyExistsException.class, () -> {
+            repo.save(product5);
+        });
+    }
+}
